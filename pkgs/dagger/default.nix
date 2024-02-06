bfe5a97fb1dab590ac4a8fb0f39d23e582ac8c2a
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2024-02-06 01:49:16 -0800

    feat(flake): added dagger output

--- pkgs/dagger/default.nix
+++ pkgs/dagger/default.nix
@@ -5,7 +5,7 @@ let
     "aarch64-darwin" = { sha256 = "sha256:1wjy4aapagxvld2y8d4bbz36xl4xy2l8xyf0wfwl0b5ps2wkn55v"; system = "darwin_arm64"; };
     "aarch64-linux" = { sha256 = ""; system = "linux_arm64"; };
     "x86_64-darwin" = { sha256 = ""; system = "darwin_amd64"; };
-    "x86_64-linux" = { sha256 = "sha256:1wjy4aapagxvld2y8d4bbz36xl4xy2l8xyf0wfwl0b5ps2wkn55v"; system = "linux_amd64"; };
+    "x86_64-linux" = { sha256 = "sha256:11c1wd0lh3yrbp4c9gcrs0f57xmmaihiszxmq7wbx8lxwfw452zh"; system = "linux_amd64"; };
   }.${s} or (throw "Unsupported system: ${s}");
   metadata = filterSystem system;
   name = "dagger";
