457a8955c9b349d4391564c4a1fd5fd88244dc8c
Hayden <22327045+hbjydev@users.noreply.github.com>
2024-05-26 23:18:28 +0100

    feat: alloy package (init at 1.1.0) (#3)
    * feat: grafana alloy package
    
    * feat: update package to 1.1.0
    
    * feat: alloy module
    
    * feat: environment files as an option
    
    * fix(alloy): include meta.mainProgram option
    
    * chore: pin against main until next release made
    
    * feat: alloy group

--- flake.nix
+++ flake.nix
@@ -8,24 +8,20 @@
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
 
-      perSystem = {
-        config,
-        self',
-        inputs',
-        pkgs,
-        system,
-        ...
-      }: {
+      flake.nixosModules = import ./nixos;
+
+      perSystem = { config, pkgs, ... }: {
         devShells = {
           default = pkgs.mkShell {
-            inputsFrom = [self'.packages.dagger];
-            nativeBuildInputs = [pkgs.just self'.packages.dagger];
+            inputsFrom = [config.packages.dagger];
+            nativeBuildInputs = [pkgs.just config.packages.dagger];
           };
         };
 
         formatter = pkgs.alejandra;
 
         packages = {
+          alloy = pkgs.callPackage ./pkgs/alloy {};
           dagger = pkgs.callPackage ./pkgs/dagger {};
           steampipe = pkgs.callPackage ./pkgs/steampipe {};
         };
